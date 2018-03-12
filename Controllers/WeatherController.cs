08bebeb91309cfc6ffd240a674638c051e3ad7bb
suneelreddychalla <saisuneelreddychalla@gmail.com>
2018-03-12 19:56:55 -0400

    api from external sources

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace projects.Controllers
 {
@@ -6,10 +12,30 @@ namespace projects.Controllers
     public class WeatherController : Controller
     {
         [HttpGet("[action]/{city}")]
-        public IActionResult City(string city)
+        public async Task<IActionResult> City(string city)
         {
-            return Ok(new { Temp = "12", Summary = "Barmy", City = city });
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("http://api.openweathermap.org");
+                    var response = await client.GetAsync($"/data/2.5/weather?q={city}&appid=02edd1677041344cb6ae77003569a316&units=metric");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject<OpenWeatherResponse>(stringResult);
+                    return Ok(new
+                    {
+                        Temp = rawWeather.Main.Temp,
+                        Summary = string.Join(",", rawWeather.Weather.Select(x => x.Main)),
+                        City = rawWeather.Name
+                    });
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                }
+            }
         }
-        sdf
     }
 }
