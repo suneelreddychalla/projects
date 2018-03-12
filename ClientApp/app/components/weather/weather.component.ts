3e066ad37214ffd61dba2090b911b846faba3317
suneelreddychalla <saisuneelreddychalla@gmail.com>
2018-03-12 16:28:40 -0400

    Initial commit

---
+++ ClientApp/app/components/weather/weather.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'weather',
+    template: require('./weather.component.html')
+})
+
+export class WeatherComponent {
+    public weather: Weather;
+
+    constructor(http: Http) {
+        http.get('/api/weather/city/london').subscribe(result => {
+            this.weather = result.json();
+        })       
+    }
+}
+
+interface Weather {
+    temp: String;
+    summary: String;
+    city: String;
+}
